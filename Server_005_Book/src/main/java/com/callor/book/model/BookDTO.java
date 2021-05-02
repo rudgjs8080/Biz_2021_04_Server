ff4d03748906975035f04182e5c9d7cc4c70884c
rudgjs8080 <kkh_8080@naver.com>
2021-05-03 00:35:48 +0900

    0503 home

--- Server_005_Book/src/main/java/com/callor/book/model/BookDTO.java
+++ Server_005_Book/src/main/java/com/callor/book/model/BookDTO.java
@@ -32,4 +32,54 @@ public class BookDTO {
 	
 	private Integer bk_price;
 	private Integer bk_pages;
+	public String getBk_isbn() {
+		return bk_isbn;
+	}
+	public void setBk_isbn(String bk_isbn) {
+		this.bk_isbn = bk_isbn;
+	}
+	public String getBk_title() {
+		return bk_title;
+	}
+	public void setBk_title(String bk_title) {
+		this.bk_title = bk_title;
+	}
+	public String getBk_cname() {
+		return bk_cname;
+	}
+	public void setBk_cname(String bk_cname) {
+		this.bk_cname = bk_cname;
+	}
+	public String getBk_cceo() {
+		return bk_cceo;
+	}
+	public void setBk_cceo(String bk_cceo) {
+		this.bk_cceo = bk_cceo;
+	}
+	public String getBk_author() {
+		return bk_author;
+	}
+	public void setBk_author(String bk_author) {
+		this.bk_author = bk_author;
+	}
+	public String getBk_au_tel() {
+		return bk_au_tel;
+	}
+	public void setBk_au_tel(String bk_au_tel) {
+		this.bk_au_tel = bk_au_tel;
+	}
+	public Integer getBk_price() {
+		return bk_price;
+	}
+	public void setBk_price(Integer bk_price) {
+		this.bk_price = bk_price;
+	}
+	public Integer getBk_pages() {
+		return bk_pages;
+	}
+	public void setBk_pages(Integer bk_pages) {
+		this.bk_pages = bk_pages;
+	}
+	
+	
 }
--- Server_005_Book/src/main/java/com/callor/book/model/BookVO.java
+++ Server_005_Book/src/main/java/com/callor/book/model/BookVO.java
@@ -22,5 +22,49 @@ public class BookVO {
 	private String bk_date;
 	private Integer bk_price = 0;
 	private Integer bk_pages = 0;
+	public String getBk_isbn() {
+		return bk_isbn;
+	}
+	public void setBk_isbn(String bk_isbn) {
+		this.bk_isbn = bk_isbn;
+	}
+	public String getBk_title() {
+		return bk_title;
+	}
+	public void setBk_title(String bk_title) {
+		this.bk_title = bk_title;
+	}
+	public String getBk_ccode() {
+		return bk_ccode;
+	}
+	public void setBk_ccode(String bk_ccode) {
+		this.bk_ccode = bk_ccode;
+	}
+	public String getBk_acode() {
+		return bk_acode;
+	}
+	public void setBk_acode(String bk_acode) {
+		this.bk_acode = bk_acode;
+	}
+	public String getBk_date() {
+		return bk_date;
+	}
+	public void setBk_date(String bk_date) {
+		this.bk_date = bk_date;
+	}
+	public Integer getBk_price() {
+		return bk_price;
+	}
+	public void setBk_price(Integer bk_price) {
+		this.bk_price = bk_price;
+	}
+	public Integer getBk_pages() {
+		return bk_pages;
+	}
+	public void setBk_pages(Integer bk_pages) {
+		this.bk_pages = bk_pages;
+	}
 
+	
+	
 }
